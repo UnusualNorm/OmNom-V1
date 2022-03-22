fb33fc5d80cd00a38206abacc1d6ad27b0962202
UnusualNorm <40722525+UnusualNorm@users.noreply.github.com>
2022-03-21 22:16:26 -0400

    Reorganize

--- src/shared/filter.ts
+++ src/shared/filter.ts
@@ -37,52 +37,58 @@ function parseFilterList(list: string[]): Filter[] {
 
 export function filter(message: Message) {
   const { channel, guild, member } = message;
+  if (!(channel.type == 'GUILD_TEXT' || channel.isThread())) return;
+  const seperate = separateThread(channel);
+
+  let filterList: Filter[] = [];
+  // Guild
+  filterList = filterList.concat(
+    parseFilterList(db.get(`guild_${guild.id}.filter.filters`))
+  );
+
+  // Roles
+  if (!message.webhookId) {
+    const roles = member.roles.cache.toJSON() || [];
+    for (let i = roles.length - 1; i >= 0; i--) {
+      const role = roles[i];
+      filterList = filterList.concat(
+        parseFilterList(
+          db.get(`guild_${guild.id}.roles.${role.id}.filter.filters`)
+        )
+      );
+    }
+  }
+
+  // Channel
+  filterList = filterList.concat(
+    parseFilterList(
+      db.get(`guild_${guild.id}.channels.${seperate.channel.id}.filter.filters`)
+    )
+  );
+
+  // Member
+  if (!message.webhookId)
+    filterList = filterList.concat(
+      parseFilterList(
+        db.get(`guild_${guild.id}.members.${member.id}.filter.filters`)
+      )
+    );
+
+  filterList.reverse();
+  if (!filterList) filterList = [];
+  if (filterList.length <= 0) return;
+
+  // Permissions
   if (
-    !(channel.type == 'GUILD_TEXT' || channel.isThread()) ||
     !channel.permissionsFor(guild.me).has('MANAGE_MESSAGES') ||
     !channel.permissionsFor(guild.me).has('MANAGE_WEBHOOKS')
   )
     return message.reply(
       'I cannot filter messages from this channel... (Do I have permissions?)'
     );
-  const seperate = separateThread(channel);
 
   ensureGetWebhook(seperate.channel, (error, webhook) => {
     if (error) return message.reply(error);
-
-    let filterList: Filter[] = [];
-    // Guild
-    filterList = filterList.concat(
-      parseFilterList(db.get(`guild_${guild.id}.filter.filters`))
-    );
-
-    // Roles
-    if (!message.webhookId) {
-      const roles = member.roles.cache.toJSON() || [];
-      for (let i = roles.length - 1; i >= 0; i--) {
-        const role = roles[i];
-        filterList = filterList.concat(
-          parseFilterList(
-            db.get(`guild_${guild.id}.roles.${role.id}.filter.filters`)
-          )
-        );
-      }
-    }
-
-    // Channel
-    filterList = filterList.concat(
-      parseFilterList(
-        db.get(`guild_${guild.id}.channels.${channel.id}.filter.filters`)
-      )
-    );
-
-    // Member
-    if (!message.webhookId)
-      filterList = filterList.concat(
-        parseFilterList(
-          db.get(`guild_${guild.id}.members.${member.id}.filter.filters`)
-        )
-      );
 
     const next = (i: number, text: string, override: WebhookMessageOptions) => {
       if (i < filterList.length)
@@ -108,10 +114,8 @@ export function filter(message: Message) {
         });
       });
     };
-    filterList.reverse();
-    if (!filterList) filterList = [];
-    if (filterList.length > 0)
-      next(0, message.content, convertMessage(message));
+
+    next(0, message.content, convertMessage(message));
   });
 }
 
